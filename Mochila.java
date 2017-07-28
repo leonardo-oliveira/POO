80d444d6d30045ca093973716a2f8db57d6a5443
leonardo-oliveira <leo-timao9@hotmail.com>
2017-07-28 12:55:43 -0300

    initial commit

---
+++ Mochila.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Mochila {
+	private int pesoMax;
+	private int pesoAt;
+	private Arraylist<Item> itens;
+	
+	public Mochila(){
+		pesoMax = 500;
+		pesoAt = 0;
+		qntMotor = 0
+		
+}
